3ad2f0c2468f113b7ada8d8cc06eefdaea53952b
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2020-12-29 19:42:05 +0000

    Update extract_data.bash

--- extract_data.bash
+++ extract_data.bash
@@ -1,5 +1,9 @@
 #! /bin/bash
-cd ./098513
+
+cd 098512
+
 #grep "Solution time =" *.txt | cut -b 1-2,4-5,28- >t0
-grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >times.txt
+grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >t1_98512
+
+mv t1_98512 ..
 
