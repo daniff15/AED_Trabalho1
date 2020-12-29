d741b49d2a38ce73a9eeb9c125be7d1ecd34983b
daniff15 <dani.fig@ua.pt>
2020-12-29 14:55:22 +0000

    asd

--- changeFolder.sh
+++ changeFolder.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-#Nome da pasta onde querem guardar os ficheiros
+#Nome da pasta onde querem guardar os ficheiros quando I=1
 nomePasta=098498_1
+#Path onde estao guardados o ficheiros
 cd /home/daniel/Desktop/AED_Trabalho1/AED_Trabalho1
+#Se nao criarem pasta antes isto cria, com o $nomePasta
 if ! [ -d $nomePasta ]; then
-    #Aqui metam o vosso path onde têm as cenas guardadas
+    #Aqui metam o vosso path onde têm as cenas guardadas+ o nome da Pasta onde vai ser guarda a info
     mkdir /home/daniel/Desktop/AED_Trabalho1/AED_Trabalho1/$nomePasta
 fi
 #Path de onde tao os ficheiros a ignorar o lucro e a nao ignorar o lucro
@@ -11,7 +13,7 @@ cd /home/daniel/Desktop/AED_Trabalho1/AED_Trabalho1/098498
 re='[0-9]{1,2}_[0-9]{1,2}_1.txt'
 for file in *; do
     if [[ $file =~ $re ]];then
-        #Path da pasta onde vao ficar guardados os ficheiros com 1 de profit
+        #Path da pasta onde vao ficar guardados os ficheiros com 1 de profit e move-os para essa pasta
         mv $file /home/daniel/Desktop/AED_Trabalho1/AED_Trabalho1/$nomePasta
     fi
 done
