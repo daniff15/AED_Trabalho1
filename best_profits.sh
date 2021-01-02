dbbda6f0fb2c92036eec0f8bee12a5e2b7310179
eva-pomposo <66091692+eva-pomposo@users.noreply.github.com>
2021-01-02 17:37:39 +0000

    Update best_profits.sh

--- best_profits.sh
+++ best_profits.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd ./098498
 
-grep "Best total profit =" *.txt | sed -e 's/_0.txt:Best total profit =//' -e 's/_/ /' >best_profits.txt
+grep "Best total profit =" *.txt | sed -e 's/_0.txt:Best total profit =//' > best_profits.txt
