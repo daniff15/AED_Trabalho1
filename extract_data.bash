f72b0be65c334accbeaec8891655d5e7d3c197e2
daniff15 <dani.fig@ua.pt>
2021-01-02 14:44:28 +0000

    ficheiros para fora

--- extract_data.bash
+++ extract_data.bash
@@ -1,13 +1,8 @@
 #! /bin/bash
-<<<<<<< HEAD
-cd ./098512
-=======
+cd 098498
 
-cd 098512
-
->>>>>>> 77cd3a10a310f10c4f6fbf384e7aefbbaaefd691
 #grep "Solution time =" *.txt | cut -b 1-2,4-5,28- >t0
-grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >t1_98512.txt
+grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >t1_98498.txt
 
-mv t1_98512.txt ..
+mv t1_98498.txt ..
 
