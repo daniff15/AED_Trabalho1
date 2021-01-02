841e8c25c343590a6b7987a44928370beba7479d
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-01-02 18:46:45 +0000

    Update best_profits.sh

--- best_profits.sh
+++ best_profits.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 cd ./098498
-
+#Retirar de cada ficheiro o lucro
+#sed serve para ir apenas aos ficheiros que terminam apenas em _0.txt, sendo estes aqueles que tem os profits
 grep "Best total profit =" *.txt | sed -e 's/_0.txt:Best total profit =//' > best_profits.txt
