a26b8c07454a17f602819eb30e2d7c7221cedd3d
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-01-02 18:49:23 +0000

    Update maxTasks.m

--- maxTasks.m
+++ maxTasks.m
@@ -2,12 +2,15 @@ clear all
 close all
 
 %Meter path onde se tem o ficheiro maxTasks guardado
+%Cada variavel fica com 3 colunas
 task98498 = load('098498\maxTasks.txt' , 'r');
 task98512 = load('098512\maxTasks.txt' , 'r');
 task98513 = load('098513\maxTasks.txt' , 'r');
 
+% Ir à terceira coluna de cada variável para retirar apenas os lucros
 maxTask498 = task98498(:,3);
 figure(1)
+% Histograma, para vermos a distribuicao do número máximo de tasks
 histogram(maxTask498)
 title('Histograma para o número máximo de tasks possíveis - 98498')
 xlabel('Número máximo de tasks possíveis')
