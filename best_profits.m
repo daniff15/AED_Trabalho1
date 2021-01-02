78e3626f4ce269cb1d2aa8b5e93943e3c794767c
daniff15 <dani.fig@ua.pt>
2021-01-02 16:45:07 +0000

    tabela, graficos e if para fora da funcao para correr mais rapido

--- best_profits.m
+++ best_profits.m
@@ -2,9 +2,9 @@ clear all
 close all
 
 %Meter path onde se tem o ficheiro best_profit guardado
-prof98498 = load('C:\Users\HP OMEN\Documents\MATLAB\AED\098498\best_profits.txt' , 'r');
-prof98512 = load('C:\Users\HP OMEN\Documents\MATLAB\AED\098512\best_profits.txt' , 'r');
-prof98513 = load('C:\Users\HP OMEN\Documents\MATLAB\AED\098513\best_profits.txt' , 'r');
+prof98498 = load('098498\best_profits.txt' , 'r');
+prof98512 = load('098512\best_profits.txt' , 'r');
+prof98513 = load('098513\best_profits.txt' , 'r');
 
 figure(1)
 profit498 = prof98498(: , 3);
@@ -27,3 +27,8 @@ title('Lucros para nº Mecanográfico - 98513');
 xlabel('Lucro');
 ylabel('Nº de Ocorrências');
 
+%% Tabelas dos bests profits
+
+tabela_98498 = table(prof98498(: , 1),prof98498(: , 2),prof98498(: , 3),'VariableNames',{'Tarefas','Programadores','Best profit'})
+tabela_98513 = table(prof98513(: , 1),prof98513(: , 2),prof98513(: , 3),'VariableNames',{'Tarefas','Programadores','Best profit'})
+tabela_98512 = table(prof98512(: , 1),prof98512(: , 2),prof98512(: , 3),'VariableNames',{'Tarefas','Programadores','Best profit'})
---
+++ graficos_tempos.m
@@ -0,0 +1,69 @@
+clear all
+close all
+
+fid = fopen('t1_98513','r');
+cell_98513 = textscan(fid,'%s %s','delimiter',newline); %cell_98513{1,1} = informação dos tempos quando I = 0
+                                                        %cell_98513{1,2} = informação dos tempos quando I = 1
+fclose(fid);
+
+fid = fopen('t1_98498','r');
+cell_98498 = textscan(fid,'%s %s','delimiter',newline); %cell_98498{1,1} = informação dos tempos quando I = 0
+                                                        %cell_98498{1,2} = informação dos tempos quando I = 1
+fclose(fid);
+
+fid = fopen('t1_98512','r');
+cell_98512 = textscan(fid,'%s %s','delimiter',newline); %cell_98512{1,1} = informação dos tempos quando I = 0
+                                                        %cell_98512{1,2} = informação dos tempos quando I = 1
+fclose(fid);
+
+%% Gráfico dos tempos de execução para I=0
+
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98513{1,1},'UniformOutput',false)) ); %n = tasks de 98513
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98513{1,1},'UniformOutput',false)) ); %p = programadores de 98513
+t = cellfun(@str2double, (cellfun(@(x) x(7:end),cell_98513{1,1},'UniformOutput',false)) ); %t = tempos de 98513
+figure
+plot3(n,p,log10(t),'r*-');
+
+hold on
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98498{1,1},'UniformOutput',false)) ); %n = tasks de 98498
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98498{1,1},'UniformOutput',false)) ); %p = programadores de 98498
+t = cellfun(@str2double, (cellfun(@(x) x(7:end),cell_98498{1,1},'UniformOutput',false)) ); %t = tempos de 98498
+plot3(n,p,log10(t),'go-');
+
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98512{1,1},'UniformOutput',false)) ); %n = tasks de 98512
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98512{1,1},'UniformOutput',false)) ); %p = programadores de 98512
+t = cellfun(@str2double, (cellfun(@(x) x(7:end),cell_98512{1,1},'UniformOutput',false)) ); %t = tempos de 98512
+plot3(n,p,log10(t),'bs-');
+
+hold off
+title('Tempos de execuçao, I=0')
+xlabel('Tarefas');
+ylabel('Programadores');
+zlabel('Tempo (s)');
+legend('98513', '98498', '98512')
+
+%% Gráfico dos tempos de execução para I=1
+
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98513{1,2},'UniformOutput',false)) ); %n = tasks de 98513
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98513{1,2},'UniformOutput',false)) ); %p = programadores de 98513
+t = cellfun(@str2double, (cellfun(@(x) x(29:end),cell_98513{1,2},'UniformOutput',false)) );  %t = tempos de 98513
+figure
+plot3(n,p,log10(t),'r*-');
+
+hold on
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98498{1,2},'UniformOutput',false)) ); %n = tasks de 98498
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98498{1,2},'UniformOutput',false)) ); %p = programadores de 98498
+t = cellfun(@str2double, (cellfun(@(x) x(29:end),cell_98498{1,2},'UniformOutput',false)) ); %t = tempos de 98498
+plot3(n,p,log10(t),'go-');
+
+n = cellfun(@str2double, (cellfun(@(x) x(1:2),cell_98512{1,2},'UniformOutput',false)) );  %n = tasks de 98512
+p = cellfun(@str2double, (cellfun(@(x) x(4:5),cell_98512{1,2},'UniformOutput',false)) ); %p = programadores de 98512
+t = cellfun(@str2double, (cellfun(@(x) x(29:end),cell_98512{1,2},'UniformOutput',false)) ); %t = tempos de 98512
+plot3(n,p,log10(t),'bs-');
+
+hold off
+title('Tempos de execuçao, I=1')
+xlabel('Tarefas');
+ylabel('Programadores');
+zlabel('Tempo (s)');
+legend('98513', '98498', '98512')
