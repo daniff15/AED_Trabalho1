c3d1e573a39ce1b08d769330fcdcc8534c7ec09b
eva-pomposo <66091692+eva-pomposo@users.noreply.github.com>
2021-01-02 17:41:25 +0000

    Update maxTasks_1.sh

--- maxTasks_1.sh
+++ maxTasks_1.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd ./098512
 
-grep "Maximum possible tasks =" *.txt | sed -e 's/_1.txt:Maximum possible tasks =//' -e 's/_/ /' >maxTasks.txt
+grep "Maximum possible tasks =" *.txt | sed -e 's/_1.txt:Maximum possible tasks =//' > maxTasks.txt
