f864c50ae8b8bab92d31e03d893ea17589403727
eva-pomposo <66091692+eva-pomposo@users.noreply.github.com>
2021-01-02 17:04:47 +0000

    Update and rename extract_data.bash to extract_data.sh

--- extract_data.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd 098498
-
-#grep "Solution time =" *.txt | cut -b 1-2,4-5,28- >t0
-grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >t1_98498.txt
-
-mv t1_98498.txt ..
-
---
+++ extract_data.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+cd 098498
+
+#grep "Solution time =" *.txt | cut -b 1-2,4-5,28- >t0
+grep "Solution time =" *.txt | sed -e 's/_0.txt:Solution time =//' -e 's/_/ /' >t1_98498
+
+mv t1_98498 ..
+
