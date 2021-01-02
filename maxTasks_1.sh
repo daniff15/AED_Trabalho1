e9494f94099635f9a197fafc31da7b9f06da5049
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-01-02 18:50:21 +0000

    Update maxTasks_1.sh

--- maxTasks_1.sh
+++ maxTasks_1.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 cd ./098512
 
+#Retirar de cada ficheiro o número máximo de tasks
+#sed serve para ir apenas aos ficheiros que terminam apenas em _1.txt, sendo estes aqueles que tem o número máximo de tasks
 grep "Maximum possible tasks =" *.txt | sed -e 's/_1.txt:Maximum possible tasks =//' > maxTasks.txt
