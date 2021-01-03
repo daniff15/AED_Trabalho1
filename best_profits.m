426d5209ac4f6210a068f126e7f47c01c35c5612
eva-pomposo <66091692+eva-pomposo@users.noreply.github.com>
2021-01-03 10:59:00 +0000

    Update best_profits.m

--- best_profits.m
+++ best_profits.m
@@ -7,6 +7,8 @@ prof98498 = load('098498\best_profits.txt' , 'r'); % Tasks  Programadores Lucro
 prof98512 = load('098512\best_profits.txt' , 'r');
 prof98513 = load('098513\best_profits.txt' , 'r');
 
+%% Histogramas
+
 figure(1)
 % Ir à terceira coluna de cada variável para retirar apenas os lucros
 profit498 = prof98498(: , 3);
