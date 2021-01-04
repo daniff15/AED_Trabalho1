59b3cf4ca877ace9d5c5216cf30d2739e4aded93
eva-pomposo <66091692+eva-pomposo@users.noreply.github.com>
2021-01-04 09:30:21 +0000

    Update total_valid_tasks.bash

--- total_valid_tasks.bash
+++ total_valid_tasks.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ./098513
-
+#Retirar de cada ficheiro o num_viables, o número de subconjuntos de tarefas válidas
+#somar todos os num_viables, de modo a obter o numero de subconjuntos de tarefas válidas para cada numero mecanográfico 
 total_viable_tasks=$( grep "Viable job selections =" *.txt | awk '{print $5}' | paste -sd+ - | bc)
 
 echo $total_viable_tasks
